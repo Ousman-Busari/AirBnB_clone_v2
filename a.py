a4938d40de285d13596e9c758483041bfb9f035e
Ousman <usman.traid@gmail.com>
2023-01-05 06:06:12 -0500

    testing vs with wsl ubuntu

---
+++ a.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def line_split(line):
+    return line.split()
+
+def main():
+    line = "create Place city_id=\"0001\" user_id=\"0001\" name=\"My_little_house\" number_rooms=4 number_bathrooms=2 max_guest=10 price_by_night=300 latitude=37.773972 longitude=-122.431297"
+    if  in line:
+        print(line.split(), "\n")
+
+if __name__ == "__main__":
+    main()
