c40501d4b01ad0b7eb8cd41c12e2135e3a65e527
Ousman <usman.traid@gmail.com>
2023-02-05 18:23:10 -0500

    hbnb live

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" starts a flask web application """
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Display all aavailabl info in a HTML body"""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    places = storage.all("Place")
+    users = storage.all("User").values()
+    return render_template("100-hbnb.html", states=states,
+                           amenities=amenities, places=places, users=users)
+
+
+@app.teardown_appcontext
+def teardown_app(exc):
+    """ closes the app session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
